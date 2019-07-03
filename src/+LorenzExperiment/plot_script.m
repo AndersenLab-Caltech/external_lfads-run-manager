dd0f329375a4e8a87982f29dcb2ae2fc34e5b24f
Charles Guan <charlesguan94@gmail.com>
2019-07-03 11:54:14 -0700

    Add spike raster

--- src/+LorenzExperiment/plot_script.m
+++ src/+LorenzExperiment/plot_script.m
@@ -1,10 +1,11 @@
 %% Load data
-run = rc.findRuns('single_dataset001', 1);
+runList = rc.findRuns('single_dataset001');
+run = runList(4);
 pm = run.loadPosteriorMeans();
 conditionId = run.loadInputInfo().conditionId;
 
 channel_to_plot = 1;
-rates1 = squeeze(pm.rates(channel_to_plot, :, :));
+rates = squeeze(pm.rates(channel_to_plot, :, :));
 
 %% Plot smoothed rates by condition index
 [unique_groups, ~, group_idx] = unique(conditionId);
@@ -12,7 +13,7 @@ cmap = jet(numel(unique_groups));
 
 figure; hold on;
 for idx = 1:numel(unique_groups)
-    plot(pm.time, rates1(:,conditionId == unique_groups(idx)),...
+    plot(pm.time, rates(:,conditionId == unique_groups(idx)),...
          'Color', cmap(idx,:))
 end
 
@@ -20,3 +21,16 @@ hold off;
 
 xlabel('Time (ms)')
 ylabel('r1');
+
+%% Plot raw spiking rates
+spikes = squeeze(run.loadInputInfo().counts(channel_to_plot,:,:));
+time = run.loadInputInfo().seq_timeVector;
+
+figure; hold on;
+imshow(spikes.')
+
+hold off;
+
+xlabel('Time (ms)')
+ylabel('spike count (1ms bin)');
+colorbar;
